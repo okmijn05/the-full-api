4d93c0dbf9b8a6fe5591ae3343fb39d3026fe51f
sonny <britzsky@gmail.com>
2025-12-17 21:21:39 +0900

    url 수정

--- src/main/java/com/example/demo/WebConfig.java
+++ src/main/java/com/example/demo/WebConfig.java
@@ -37,7 +37,7 @@ public class WebConfig implements WebMvcConfigurer {
 	
 	@Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler(DEV_HANDLE)
-		.addResourceLocations(DEV_PATH);
+		registry.addResourceHandler(REAL_HANDLE)
+		.addResourceLocations(REAL_PATH);
     }
 }
