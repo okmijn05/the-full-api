820ffc26dda372163cc46036b1e79dda9be9a0ee
wonu <938274@naver.com>
2026-01-19 11:49:35 +0900

    회원가입 승인 알림 수정

--- src/main/java/com/example/demo/mapper/UserMapper.java
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -11,13 +11,8 @@ public interface UserMapper {
 	  Map<String, Object> Login(Map<String, Object> paramMap);
 	  int UserRgt(Map<String, Object> paramMap);
 	  int UserRgtDetail(Map<String, Object> paramMap);
-
-	  // ✅ 승인대기 목록 조회(use_yn='N')
 	  List<Map<String, Object>> SelectApprovalPendingUsers(Map<String, Object> paramMap);
-	  // ✅ 승인 처리 저장용
 	  int UpdateUserUseYn(Map<String, Object> paramMap);
-
-	  int UpdateUseYn(Map<String, Object> param);
 	  List<Map<String, Object>> ApprovalPendingList();
 	  List<Map<String, Object>> SelectUserInfo(Map<String, Object> paramMap);
 	  List<Map<String, Object>> UserRecordSheetList(Map<String, Object> paramMap);
